2e9e27ee6220589590c930da1d6998b00aa187dd
Miguel Rosales <miguelrosales2027@gmail.com>
2023-01-17 18:26:55 -0800

    finished waldo, mouse follow and started sleepy head

--- _02_boolean/_b_sleepy_head.py
+++ _02_boolean/_b_sleepy_head.py
@@ -2,11 +2,46 @@
 Use boolean variables to control program logic between two different code
 paths.
 """
+from tkinter import simpledialog, messagebox
+import turtle
+from turtle import Turtle
 
 
 if __name__ == '__main__':
-    # TODO)
+
+
+
+    is_weekend = False
+    day_week =  simpledialog.askstring(None,"What day is it?")
+
     #  1. Use a boolean variable to indicate if it is the weekend.
+    if day_week == "Saturday" or day_week == "Sunday":
+        is_weekend = True
+        messagebox.showinfo(None,"Have a great Weekend!")
+    else:
+        messagebox.showinfo(None,'almost there!')
+
+    if is_weekend:
+        test_quest = simpledialog.askstring(None,"did you pass all your test?")
+    if test_quest == "yes":
+        messagebox.showinfo(None,"Great Job!")
+    else:
+        messagebox.showinfo(None,"Study more! ")
+    if is_weekend:
+        game_on = simpledialog.askstring(None, "Is the game on?")
+
+    if game_on == "yes":
+        messagebox.showinfo(None,"Have fun!")
+    else:
+        messagebox.showinfo(None,"Can't Wait!")
+
+    if is_weekend:
+        red_color= simpledialog.showinfo(None,"Should the shape be red?")
+
+    if red_color == "yes":
+        window = turtle.Screen()
+        window.bgcolor('white')
+
     #     Display a different message to the user depending on whether it is
     #     the weekend or not.
     #  2. Use a boolean variable to indicate if a student passed an exam.
