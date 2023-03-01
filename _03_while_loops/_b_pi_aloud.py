daf56687498576455be71e0e6c738c1b3f4ec61d
Miguel Rosales <miguelrosales2027@gmail.com>
2023-02-28 18:29:27 -0800

    finished waldo, mouse follow and started sleepy head

--- _03_while_loops/_b_pi_aloud.py
+++ _03_while_loops/_b_pi_aloud.py
@@ -11,9 +11,26 @@ if __name__ == '__main__':
     # TODO) Place the first 20 digits of pi into a variable as a string
     #  3.1415926535897932384
 
+    pi='3.1415926535897932384'
+
     # TODO) Print out the first 3 digits of pi. For example,
     #  pi_str[0]   # first digit
     #  pi_str[1]   # second digit
+
+    print(pi[0])
+    print(pi[1])
+    print(pi[2])
+    print(pi[3])
+
+    pguess=4
+    while True:
+        useranswer=simpledialog.askstring(title=None,prompt="What are the next digits?")
+        if useranswer==pi[pguess]:
+            print(pi[pguess])
+            pguess=pguess+1
+
+        else:
+            break
 
     # TODO) Use a while loop to keep asking for the next digit of pi
 
